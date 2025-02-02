768e92f2e987c5e0b96501dba8c12c0cb87bf34a
hybidmatches <miha.daytron@gmail.com>
2025-02-02 20:44:19 +0100

    topics built

--- src/build.sh
+++ src/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 site=../site
 content=content
-headerA=../../inc/header-top.htm
-headerB=../../inc/header-bottom.htm
-sitenav=../../inc/nav.htm
+headerA=../../../inc/header-top.htm
+headerB=../../../inc/header-bottom.htm
+sitenav=../../../inc/nav.htm
 meta=meta.htm
 contentFile=content.htm
-foot=../../inc/footer.htm
-bottom=../../inc/html-bottom.htm
+foot=../../../inc/footer.htm
+bottom=../../../inc/html-bottom.htm
 tally=0
 
 rm -rf $site
@@ -29,22 +29,38 @@ setupindex() {
 	echo "Index built"
 }
 
+sitenav() {
+	echo "<nav class='sitenav'>" > ../inc/nav.htm;
+	for f in *; do
+		if [ $f != 'index' ]; then
+			echo "<a href='${f}.html'>{${f}}</a>" >>../inc/nav.htm;
+		fi
+	done
+	echo "</nav>" > ../inc/nav.htm;
+	echo "nav"
+}
+
 # Setup topics
 cd $content
+sitenav;
 setupindex;
 for f in *; do
 	cd $f;
-	markup=''
-	topPart=$(cat $headerA $meta $headerB);
-	nav=$(cat $sitenav);
-	contentText=$(cat $contentFile);
-	footer=$(cat $foot);
-	closefile=$(cat $bottom);
-	mainContent="<main>${contentText}</main>";
-	sideBar="<aside>${markup}</aside>";
-	echo ${topPart}${nav}"${mainContent}"${sideBar}${footer}${closefile} > ../../$site/${f}.html
+	for f in *; do
+		cd $f;
+		markup=''
+		topPart=$(cat $headerA $meta $headerB);
+		nav=$(cat $sitenav);
+		contentText=$(cat $contentFile);
+		footer=$(cat $foot);
+		closefile=$(cat $bottom);
+		mainContent="<main>${contentText}</main>";
+		sideBar="<aside>${markup}</aside>";
+		echo ${topPart}${nav}"${mainContent}"${sideBar}${footer}${closefile} > ../../../$site/${f}.html
+		cd ..
+		tally=$((tally+1))
+	done
 	cd ..
-	tally=$((tally+1))
 done
 echo "${tally} topics built"
 
